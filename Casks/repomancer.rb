ea4994d923dd095bbb134e653d96819f81feedad
Jason Short <jason@sheersky.com>
2025-06-15 11:42:52 -0500

    Added Repomancer cask

--- Casks/repomancer.rb
+++ Casks/repomancer.rb
@@ -1,7 +1,7 @@
 cask "repomancer" do
   version "0.0.1"
   sha256 "6e514188f549598f166d6dcb6f8df29f333e954e28172a2901ea988a14f2647"
-  url url "https://github.com/repomancer/repomancer/releases/download/v#{version}/Repomancer-#{version}-Darwin-arm64.dmg"
+  url "https://github.com/repomancer/repomancer/releases/download/v#{version}/Repomancer-#{version}-Darwin-arm64.dmg"
   name "Repomancer"
   desc "Manage mass changes across repositories"
   homepage "https://github.com/repomancer/repomancer"
